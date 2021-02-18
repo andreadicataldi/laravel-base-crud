18227acf3e319a7cb3dd1d9c1fff46e409cfee53
Andrea Di Cataldi <andrea.dicataldi@gmail.com>
2021-02-18 16:05:26 +0100

    added summary blade

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Post;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        // Show create post form
+        return view('posts.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        // Validate posted form data
+        $validated = $request->validate([
+            'title' => 'required|string|unique:posts|min:5|max:100',
+            'content' => 'required|string|min:5|max:2000',
+            'category' => 'required|string|max:30'
+        ]);
+
+        // Create slug from title
+        $validated['slug'] = Str::slug($validated['title'], '-');
+
+        // Create and save post with validated data
+        $post = Post::create($validated);
+
+        // Redirect the user to the created post with a success notification
+        return redirect(route('posts.show', [$post->slug]))->with('notification', 'Post created!');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Post $post)
+    {
+        // Pass current post to view
+        return view('posts.show', compact('post'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Post $post)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Post $post)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Post $post)
+    {
+        //
+    }
+}
